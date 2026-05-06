916b235499becc2e1180dd72c92b3329652de3a7
fozaza <pannavit2550game@gmail.com>
2026-05-06 20:34:17 +0700

    update battery charging state

--- i3/script/runner.sh
+++ i3/script/runner.sh
@@ -5,6 +5,7 @@ pkill memory_update
 pkill read_memory
 pkill battery_dunstify_sh
 pkill dunstify_routine_sh
+pkill battery_charger_duntify_sh.sh
 pkill feh
 
 export LOACL_MEM=$HOME/.local/share/local_mem/
@@ -13,5 +14,6 @@ polybar --config=$HOME/.config/polybar/custom/config.ini &
 $HOME/.config/i3/script/display/memory_update.sh &
 $HOME/.config/i3/script/display/battery_dunstify_sh.sh &
 $HOME/.config/i3/script/display/dunstify_routine_sh.sh &
-$HOME/.config/bgrunner/bgChosse
+$HOME/.config/i3/script/display/battery_charger_duntify_sh.sh &
+$HOME/.config/bgrunner/bgChosse &
 $HOME/.config/i3/script/display/read_memory -t1 &
