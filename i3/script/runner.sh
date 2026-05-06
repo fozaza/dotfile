e87868f5ac2dfa2014ac74d651eb0e8bf26dc317
fozaza <pannavit2550game@gmail.com>
2026-05-06 20:14:51 +0700

    update gvfs,dunst_routine and fix file i3/script/runner

--- i3/script/runner.sh
+++ i3/script/runner.sh
@@ -3,9 +3,15 @@
 pkill polybar
 pkill memory_update
 pkill read_memory
+pkill battery_dunstify_sh
+pkill dunstify_routine_sh
+pkill feh
 
 export LOACL_MEM=$HOME/.local/share/local_mem/
 
 polybar --config=$HOME/.config/polybar/custom/config.ini &
 $HOME/.config/i3/script/display/memory_update.sh &
-$HOME/.config/i3/script/display/read_memory -t1
+$HOME/.config/i3/script/display/battery_dunstify_sh.sh &
+$HOME/.config/i3/script/display/dunstify_routine_sh.sh &
+$HOME/.config/bgrunner/bgChosse
+$HOME/.config/i3/script/display/read_memory -t1 &
